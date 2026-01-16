cb4ecd00dfbedc321f5a518b470d06184827bc2c
Oliver Maglana <jamols09@gmail.com>
2026-01-16 17:09:44 +0800

    ~ add a little bit of validation

--- backend/src/routes/agent.routes.ts
+++ backend/src/routes/agent.routes.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from "express";
 import { agents } from "../models/agentStore";
 import { PropertyAgent } from "../models/PropertyAgent";
 import {v4 as uuidv4} from 'uuid';
+import validator from "validator";
 
 const router = Router();
 
@@ -13,6 +14,13 @@ router.post("/", (req: Request, res: Response) => {
 
     // Check if mail already exists
     const emailExists = agents.some(a => a.email === email);
+
+    if (!validator.isEmail(email)) {
+        console.log(email);
+        console.log(validator.isEmail(email))
+        return res.status(400).json({ message: "Invalid email format" });
+    }
+
     if (emailExists) {
         return res.status(409).json({ message: "Email already exists" });
     }
@@ -39,7 +47,12 @@ router.get("/", (_req: Request, res: Response) => {
 
 //Show One :id
 router.get("/:id", (req: Request, res: Response) => {
+    const agent = agents.find(a => a.id === req.params.id);
 
+    if (!agent) {
+        return res.status(404).json({ message: "Agent not found" });
+    }
+    res.json(agent);
 });
 
 //Update Item

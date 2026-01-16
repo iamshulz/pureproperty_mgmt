3e099e91b8be8315169b5ae06e7c77a34649fbe1
Oliver Maglana <jamols09@gmail.com>
2026-01-16 15:20:41 +0800

    ~ setup route structure

--- backend/routes/agent.routes.ts
+++ backend/routes/agent.routes.ts
@@ -1,6 +1,34 @@
 import { Router, Request, Response } from "express";
-
+import { agents } from "../models/agenStore";
+import { PropertyAgent } from "../models/PropertyAgent";
 
 const router = Router();
+
+/**
+ * CREATE
+ */
+router.post("/", (req: Request, res: Response) => {
+
+});
+
+//SHOW ALL
+router.get("/", (_req: Request, res: Response) => {
+
+});
+
+//Show One :id
+router.get("/:id", (req: Request, res: Response) => {
+
+});
+
+//Update Item
+router.put("/:id", (req: Request, res: Response) => {
+
+});
+
+/// Delte Item
+router.delete("/:id", (req: Request, res: Response) => {
+
+});
 
 export default router;

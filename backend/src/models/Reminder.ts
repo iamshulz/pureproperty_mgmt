0f2ae20fdb813b3a13e558bdc411e6fe10079b40
Oliver Maglana <jamols09@gmail.com>
2026-01-16 20:37:16 +0800

    ~ fix typo

--- backend/src/models/Reminder.ts
+++ backend/src/models/Reminder.ts
@@ -1,7 +1,7 @@
 export interface Reminder {
     id: string;
     agentId: string;
-    propertId: string;
+    propertyId: string;
     eventDate: string;
     isCompleted: boolean;
     createdAt: Date;
